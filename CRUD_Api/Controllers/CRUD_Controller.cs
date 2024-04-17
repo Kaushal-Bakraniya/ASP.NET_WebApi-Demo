cc59734f1343a97195bb06653e222927341a8a56
Kaushal Bakraniya <kaushalbakraniya90@gmail.com>
2024-04-17 11:11:35 +0530

    asp.net webapi crud demo

---
+++ CRUD_Api/Controllers/CRUD_Controller.cs
@@ -0,0 +1,72 @@
+﻿using CRUD_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CRUD_Controller : ControllerBase
+    {
+        private readonly ILogger<CRUD_Controller> _logger;
+
+        public CRUD_Controller(ILogger<CRUD_Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("GetAllRecords")]
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok(JsonConvert.SerializeObject(UserModel.getAllRecords()));
+        }
+
+        [Route("InsertRecord")]
+        [HttpPost]
+        public IActionResult Insert(string name, string email)
+        {
+            int res = UserModel.InsertRecord(name,email);
+
+            if(res == 0)
+            {
+                return Ok("Executed : Something Went Wrong . . .");
+            }
+
+            return Ok("Executed : Record Inserted Successfully.");
+        }
+
+        [Route("UpdateRecord")]
+        [HttpPut]
+        public IActionResult Update(int id, string name, string email)
+        {
+            int res = UserModel.UpdateRecord(name, email, id);
+
+            if (res == 0)
+            {
+                return Ok("Executed : Something Went Wrong . . .");
+            }
+
+            return Ok("Executed : Record Updated Successfully.");
+        }
+
+        [Route("DeleteRecord")]
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            int res = UserModel.DeleteRecord(id);
+
+            if (res == 0)
+            {
+                return Ok("Executed : Something Went Wrong . . .");
+            }
+
+            return Ok("Executed : Record Deleted Successfully.");
+        }
+    }
+}
---
+++ CRUD_Api/Models/UserModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace CRUD_Api.Models
+{
+    public class UserModel
+    {
+        public UserModel()
+        {
+
+        }
+
+        static string connectionString = @"ADD_YOUR_CONNECTION_STRING_HERE";
+
+        static SqlConnection connection = new SqlConnection(connectionString);
+
+        public static DataTable getAllRecords()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                connection.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter("select * from tbl_userdeatils", connection);
+
+                da.Fill(dt);
+            }
+            catch (Exception x)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+        public static int InsertRecord(string name, string email)
+        {
+            int res = 0;
+
+            try
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("insert into tbl_userdeatils(name, email) values(@name,@email)", connection);
+
+                cmd.Parameters.AddWithValue("@name",name);
+                cmd.Parameters.AddWithValue("@email", email);
+
+                res = cmd.ExecuteNonQuery();
+            }
+            catch (Exception x)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return res;
+        }
+
+        public static int UpdateRecord(string name, string email, int id)
+        {
+            int res = 0;
+
+            try
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("update tbl_userdeatils set name = @name, email=@email where id = @id", connection);
+
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                res = cmd.ExecuteNonQuery();
+            }
+            catch (Exception x)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return res;
+        }
+
+        public static int DeleteRecord(int id)
+        {
+            int res = 0;
+
+            try
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("delete from tbl_userdeatils where id = @id", connection);
+
+                cmd.Parameters.AddWithValue("@id", id);
+
+                res = cmd.ExecuteNonQuery();
+            }
+            catch (Exception x)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return res;
+        }
+    }
+}
